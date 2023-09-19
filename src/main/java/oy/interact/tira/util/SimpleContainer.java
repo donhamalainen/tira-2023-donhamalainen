5cc089ce54e49f1a391f6ac999f3476045171e2c
Don <hamalainen.don@gmail.com>
2023-09-19 12:34:59 +0300

    Update SimpleContainer.java

--- src/main/java/oy/interact/tira/util/SimpleContainer.java
+++ src/main/java/oy/interact/tira/util/SimpleContainer.java
@@ -192,6 +192,8 @@ public class SimpleContainer<E extends Comparable<E>> implements TIRAContainer<E
 	@Override
 	public void sort() {
 		// TODO: Student: finish this as part of task 02.
+		Algorithms.insertionSort(this.array);
+		sorted = true;
 	}
 
 	// TEACHERS: TODO: Remove the call to Algorithms sort method.
